3acab44eb12ea76208df63084b33531f343bf238
Gopalachari Bansidhar <bansidhar.g@gmail.com>
2020-09-13 22:21:37 -0700

    Solution to problem 3

--- python_solutions/problem_0003.py
+++ python_solutions/problem_0003.py
@@ -6,8 +6,24 @@ What is the largest prime factor of the number 600851475143 ?
 https://projecteuler.net/problem=3
 '''
 
+import math
+
+def get_max_prime_factor(n):
+  m = 0
+  while not n%2:
+    m = 2
+    n = n/2
+
+  for i in range(3, int(math.sqrt(n)+1), 2):
+    if not n%i:
+      m = i
+      n = n/i
+  return m
+
 def main():
-  pass
+  n = 600851475143
+  m = get_max_prime_factor(n)
+  print (m)
   
 if __name__ == "__main__":
     main()
